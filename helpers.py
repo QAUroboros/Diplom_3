9d06fc6eef27076d9122cd60e4b42381db4d3636
Artem Krivoshein <artem.krivoshein@tele2.kz>
2024-10-24 22:54:12 +0700

    изменил параметр в helpers

--- helpers.py
+++ helpers.py
@@ -4,6 +4,6 @@ import faker
 def get_faker_user() -> dict:
     fake = faker.Faker()
     email = fake.email()
-    password = fake.password(length=8, special_chars=True, digits=True, upper_case=True, lower_case=True)
+    password = fake.password(length=15, special_chars=True, digits=True, upper_case=True, lower_case=True)
     name = fake.name()
     return {"email": email, "password": password, "name": name}
