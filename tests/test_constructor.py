f587ba9456fd0cf774c7bba9d3201044a2aa3d92
Artem Krivoshein <artem.krivoshein@tele2.kz>
2024-10-24 20:38:12 +0700

    убрал не используемую зависимость

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,4 +1,3 @@
-import time
 import allure
 from page.home_page import HomePage
 from page.ingridient_details_page import IngredientDetailsPage
@@ -11,9 +10,7 @@ class TestConstructor:
         home_page = HomePage(open_browser)
         ingredient_details = IngredientDetailsPage(open_browser)
         home_page.open_home_page()
-        print("Home page opened")
         home_page.click_fluorescent_bun()
-        print("Clicked on fluorescent bun")
         assert ingredient_details.is_modal_open(), "Модальное окно не открылось"
         name_ingredient = home_page.get_fluorescent_bun_name()
         name_ingredient_in_modal = ingredient_details.get_ingredient_name_in_modal()
